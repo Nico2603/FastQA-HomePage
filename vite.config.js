1c6f97e551b95bd38458d233015175e5d688a09b
Nico2603 <nicolasceballosbrito@gmail.com>
2025-02-22 22:22:21 -0500

    Fix: Mover build a docs/ para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/FastQA-HomePage/",  // Asegura que este sea el nombre exacto del repo en GitHub
+  base: "/FastQA-HomePage/",  // Asegura que sea el nombre exacto del repo en GitHub
   build: {
-    outDir: "dist",            // GitHub Pages leerá desde "dist/"
-    emptyOutDir: true,         // Limpia "dist/" antes de cada build
+    outDir: "docs",           // GitHub Pages solo permite /docs
+    emptyOutDir: true,        // Limpia "docs/" antes de cada build
   },
 });
