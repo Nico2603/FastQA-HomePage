6ad4485b27628429bd45c190757ede5ee978ebf7
Nico2603 <nicolasceballosbrito@gmail.com>
2025-02-22 22:14:30 -0500

    Fix: Configuración para despliegue en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/FastQA-HomePage/", // Nombre exacto de tu repo
+  base: "/FastQA-HomePage/",  // Asegura que este sea el nombre exacto del repo en GitHub
   build: {
-    outDir: "docs",    // <<--- El build final va a esta carpeta
-    emptyOutDir: true, // Limpia "docs/" antes de cada build
+    outDir: "dist",            // GitHub Pages leerá desde "dist/"
+    emptyOutDir: true,         // Limpia "dist/" antes de cada build
   },
 });
